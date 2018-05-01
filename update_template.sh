cd9f9b5154bf2b529b6cd95836b26d30ad97df9b
Matthew Feickert <mfeickert@smu.edu>
2018-05-01 15:40:30 +0200

    Use thesis submodule files directly

--- update_template.sh
+++ update_template.sh
@@ -4,17 +4,24 @@ function install {
     # In case using an older version of Git
     git submodule update --init --recursive
     git submodule update --recursive
-    rsync -r --update --exclude=.git* --exclude=*.sh --exclude=*.md --exclude=LICENSE Dedman-Thesis-Latex-Template/* .
+
+    # Install files for the user to edit
+    rsync -r --update Dedman-Thesis-Latex-Template/user_thesis.tex .
+    rsync -r --update Dedman-Thesis-Latex-Template/latex/user_commands.tex ./latex/user_commands.tex
+    rsync -r --update Dedman-Thesis-Latex-Template/images .
+    rsync -r --update Dedman-Thesis-Latex-Template/bib .
+    rsync -r --update Dedman-Thesis-Latex-Template/Makefile .
+
+    # Use the template base files
+    sed -i 's/sty\//Dedman-Thesis-Latex-Template\/sty/g' user_thesis.tex
+    sed -i 's/latex\/packages/Dedman-Thesis-Latex-Template\/latex\/packages/g' user_thesis.tex
+    sed -i 's/latex\/preamble/Dedman-Thesis-Latex-Template\/latex\/preamble/g' user_thesis.tex
+    sed -i 's/latex\/custom_commands/Dedman-Thesis-Latex-Template\/latex\/custom_commands/g' user_thesis.tex
+    sed -i 's/latex\/standalone_abstract/Dedman-Thesis-Latex-Template\/latex\/standalone_abstract/g' Makefile
 }
 
 function update {
     git submodule update --recursive --remote
-    # Update only if the submodule's have been updated more recently then user's
-    rsync -r --update Dedman-Thesis-Latex-Template/sty .
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/packages.tex ./latex/packages.tex
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/custom_commands.tex ./latex/custom_commands.tex
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/preamble.tex ./latex/preamble.tex
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/standalone_abstract.tex ./latex/standalone_abstract.tex
 }
 
 function main {
