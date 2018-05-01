4c0ec4424abfa47b24846bf94153923c36d0a9f1
Matthew Feickert <matthew.feickert@cern.ch>
2018-05-01 19:44:38 +0200

    Use thesis submodule files directly (#13)
    * Use thesis submodule files directly
    
    At the moment standalone_abstract must still be treated as a user
    editable file.
    
    * Add CI check to insure installer works as a user would use it
    
    * Update README

--- update_template.sh
+++ update_template.sh
@@ -4,17 +4,30 @@ function install {
     # In case using an older version of Git
     git submodule update --init --recursive
     git submodule update --recursive
-    rsync -r --update --exclude=.git* --exclude=*.sh --exclude=*.md --exclude=LICENSE Dedman-Thesis-Latex-Template/* .
+
+    # Install files for the user to edit
+    rsync -r --update Dedman-Thesis-Latex-Template/user_thesis.tex .
+    if [ ! -d "latex" ]; then
+        mkdir latex
+    fi
+    rsync -r --update Dedman-Thesis-Latex-Template/latex/user_commands.tex ./latex/
+    rsync -r --update Dedman-Thesis-Latex-Template/latex/FrontPages.tex ./latex/
+    rsync -r --update Dedman-Thesis-Latex-Template/latex/standalone_abstract.tex ./latex/
+    rsync -r --update Dedman-Thesis-Latex-Template/src .
+    rsync -r --update Dedman-Thesis-Latex-Template/images .
+    rsync -r --update Dedman-Thesis-Latex-Template/bib .
+    rsync -r --update Dedman-Thesis-Latex-Template/Makefile .
+
+    # Use the template base files
+    sed -i 's/sty\//Dedman-Thesis-Latex-Template\/sty\//g' user_thesis.tex
+    sed -i 's/latex\/packages/Dedman-Thesis-Latex-Template\/latex\/packages/g' user_thesis.tex
+    sed -i 's/latex\/packages/Dedman-Thesis-Latex-Template\/latex\/packages/g' latex/standalone_abstract.tex
+    sed -i 's/latex\/preamble/Dedman-Thesis-Latex-Template\/latex\/preamble/g' user_thesis.tex
+    sed -i 's/latex\/custom_commands/Dedman-Thesis-Latex-Template\/latex\/custom_commands/g' user_thesis.tex
 }
 
 function update {
     git submodule update --recursive --remote
-    # Update only if the submodule's have been updated more recently then user's
-    rsync -r --update Dedman-Thesis-Latex-Template/sty .
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/packages.tex ./latex/packages.tex
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/custom_commands.tex ./latex/custom_commands.tex
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/preamble.tex ./latex/preamble.tex
-    rsync -r --update Dedman-Thesis-Latex-Template/latex/standalone_abstract.tex ./latex/standalone_abstract.tex
 }
 
 function main {
@@ -23,13 +36,13 @@ function main {
         install
     elif [[ "$1" == "update" ]]; then
         update
-        echo "Updated base files"
+        echo "Updated base files. Please commit changes to .gitmodules"
     else
         printf "Do you want to sync your base LaTeX files with the template? [Y/N]: "
         read -r response
         if [[ "${response,,}" == "y" ]]; then
             update
-            echo "Updated base files"
+            echo "Updated base files. Please commit changes to .gitmodules"
         fi
     fi
 }
